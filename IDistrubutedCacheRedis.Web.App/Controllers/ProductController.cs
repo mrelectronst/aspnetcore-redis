138587abf200bac53cae3daf1e2d22f5f18eb707
Fatih Karaman <fatih.karaman.dn@aygaz.com.tr>
2022-06-08 15:16:19 +0300

    Add project files.

---
+++ IDistrubutedCacheRedis.Web.App/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistrubutedCacheRedis.Web.App.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IDistributedCache _distrubutedCache;
+
+        public ProductController(IDistributedCache distrubutedCache)
+        {
+            _distrubutedCache = distrubutedCache;
+        }
+        public IActionResult Index()
+        {
+            DistributedCacheEntryOptions cacheOptions = new DistributedCacheEntryOptions();
+
+            cacheOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(2);
+
+            cacheOptions.SlidingExpiration = TimeSpan.FromSeconds(60);
+
+            _distrubutedCache.SetString("name", "john", cacheOptions);
+
+            return View();
+        }
+
+        public IActionResult ShowCache()
+        {
+            ViewBag.Name = _distrubutedCache.GetString("name");
+
+            return View();
+        }
+
+        public IActionResult RemoveCache()
+        {
+            _distrubutedCache.Remove("name");
+
+            return View();
+        }
+    }
+}
---
+++ IDistrubutedCacheRedis.Web.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IDistrubutedCacheRedis.Web.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
